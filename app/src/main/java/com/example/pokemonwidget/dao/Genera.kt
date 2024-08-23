44929c239463e598d034762f892df402fff6eb8c
ShinWoon <brandy.hwang@gmail.com>
2024-08-23 16:06:16 +0900

    ✨ Dao 값 생성

---
+++ app/src/main/java/com/example/pokemonwidget/dao/Genera.kt
@@ -0,0 +1,11 @@
+package com.example.pokemonwidget.dao
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Genera(
+    @SerializedName("genus")
+    val genus: String,
+    @SerializedName("language")
+    val language: Language
+)
