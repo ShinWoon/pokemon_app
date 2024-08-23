39c52857b8db13632095151d948f85106347fa19
ShinWoon <brandy.hwang@gmail.com>
2024-08-23 17:54:16 +0900

    💄 PokeAppWidget ui 수정

--- app/src/main/java/com/example/pokemonwidget/PokeAppWidget.kt
+++ app/src/main/java/com/example/pokemonwidget/PokeAppWidget.kt
@@ -1,15 +1,50 @@
 package com.example.pokemonwidget
 
 import android.content.Context
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
 
-class PokeAppWidget: GlanceAppWidget() {
+class PokeAppWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Text(text = "Hello World")
+            GlanceTheme {
+                PokeWidgetContent()
+            }
+        }
+    }
+
+    @Composable
+    private fun PokeWidgetContent(
+        modifier: GlanceModifier = GlanceModifier
+    ) {
+        Row(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column {
+
+            }
+            Column {
+
+            }
+        }
+    }
+
+
+    @Preview
+    @Composable
+    fun PokeWidgetContentPreview() {
+        GlanceTheme {
+            PokeWidgetContent()
         }
     }
 }
