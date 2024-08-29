c2659069f513376920df69858f9335cfd078611c
ShinWoon <brandy.hwang@gmail.com>
2024-08-29 17:12:21 +0900

    ✨ app widget 화면 수정

--- app/src/main/java/com/example/pokemonwidget/PokeAppWidget.kt
+++ app/src/main/java/com/example/pokemonwidget/PokeAppWidget.kt
@@ -1,17 +1,23 @@
 package com.example.pokemonwidget
 
 import android.content.Context
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
 import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.layout.size
 
 class PokeAppWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
@@ -24,18 +30,34 @@ class PokeAppWidget : GlanceAppWidget() {
 
     @Composable
     private fun PokeWidgetContent(
-        modifier: GlanceModifier = GlanceModifier
+        glanceModifier: GlanceModifier = GlanceModifier,
     ) {
         Row(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = glanceModifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp, vertical = 16.dp)
+                .background(R.color.purple_200),
+            horizontalAlignment = Alignment.Start,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
+            PokeImgContent()
             Column {
 
             }
-            Column {
+        }
+    }
 
-            }
+    @Composable
+    private fun PokeImgContent(
+        modifier: GlanceModifier = GlanceModifier,
+    ) {
+        Box(
+            modifier = modifier
+                .size(width = 72.dp, height = 96.dp)
+                .background(R.color.teal_700)
+                .cornerRadius(120.dp),
+        ) {
+
         }
     }
 
