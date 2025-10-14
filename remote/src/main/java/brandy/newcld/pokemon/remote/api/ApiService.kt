97d31b668acef28b0cb72e0653ea0974c3ebfa95
shinwoon <brandy.hwang@gmail.com>
2025-10-14 14:02:49 +0900

    ✨ pokemon 리스트 호출 api 및 response 모델 추가

--- remote/src/main/java/brandy/newcld/pokemon/remote/api/ApiService.kt
+++ remote/src/main/java/brandy/newcld/pokemon/remote/api/ApiService.kt
@@ -1,14 +1,22 @@
 package brandy.newcld.pokemon.remote.api
 
+import brandy.newcld.pokemon.remote.model.PokemonInfoResponse
 import brandy.newcld.pokemon.remote.model.PokemonResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("pokemon/{id}")
-    suspend fun getPokemon(
-        @Path("id") id: Int
+    /* 포켓몬 리스트 호출 */
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
     ): PokemonResponse
 
-
+    /* 선택 포켓몬 상세 정보 호출 */
+    @GET("pokemon/{id}")
+    suspend fun getPokemonInfo(
+        @Path("id") id: Int
+    ): PokemonInfoResponse
 }
--- remote/src/main/java/brandy/newcld/pokemon/remote/model/PokemonResponse.kt
+++ remote/src/main/java/brandy/newcld/pokemon/remote/model/PokemonResponse.kt
@@ -4,38 +4,12 @@ package brandy.newcld.pokemon.remote.model
 import com.google.gson.annotations.SerializedName
 
 data class PokemonResponse(
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("name")
-    val name: String,
-    @SerializedName("base_experience")
-    val baseExperience: Int,
-    @SerializedName("height")
-    val height: Int,
-    @SerializedName("is_default")
-    val isDefault: Boolean,
-    @SerializedName("order")
-    val order: Int,
-    @SerializedName("weight")
-    val weight: Int,
-    @SerializedName("abilities")
-    val abilities: List<AbilitiesResponse>,
-    @SerializedName("forms")
-    val forms: List<NameUrlResponse>,
-    @SerializedName("held_items")
-    val heldItems: List<HeldItemResponse>,
-    @SerializedName("location_area_encounters")
-    val locationAreaEncounters: String,
-    @SerializedName("moves")
-    val moves: List<MovesResponse>,
-    @SerializedName("species")
-    val species: NameUrlResponse,
-    @SerializedName("sprites")
-    val sprites: SpritesResponse,
-    @SerializedName("cries")
-    val cries: CriesResponse,
-    @SerializedName("stats")
-    val stats: List<StatsResponse>,
-    @SerializedName("types")
-    val types: List<TypesResponse>
+    @SerializedName("count")
+    val count: Int,
+    @SerializedName("next")
+    val next: String,
+    @SerializedName("previous")
+    val previous: Any,
+    @SerializedName("results")
+    val results: List<NameUrlResponse>
 )
